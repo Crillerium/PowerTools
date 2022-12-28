ea4532763b5fde5c83bf5d24eb62ec8eb756ec63
Crillerium <96243292+Crillerium@users.noreply.github.com>
2022-12-28 22:13:27 +0800

    Add files via upload

---
+++ ServerNess/openport.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import os
+import getpass
+if getpass.getuser() == 'root':
+    os.system('systemctl stop firewalld.service 2>/dev/null')
+    os.system('systemctl disable firewalld.service 2>/dev/null')
+    os.system('setenforce 0 2>/dev/null')
+    os.system('ufw disable 2>/dev/null')
+    os.system('iptables -P INPUT ACCEPT 2>/dev/null')
+    os.system('iptables -P FORWARD ACCEPT 2>/dev/null')
+    os.system('iptables -P OUTPUT ACCEPT 2>/dev/null')
+    os.system('iptables -t nat -F 2>/dev/null')
+    os.system('iptables -t mangle -F 2>/dev/null')
+    os.system('iptables -F 2>/dev/null')
+    os.system('iptables -X 2>/dev/null')
+    os.system('netfilter-persistent save 2>/dev/null')
+    print("\033[1;32m提示:已开放系统所有端口\033[0m")
+else:
+    print("\033[1;32m提示:请以root权限运行\033[0m")
